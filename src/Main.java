9c91e99bc0ff854c59dab0c2a10b273ecabfd118
fred <fred.nobre@octo.com>
2022-12-22 16:51:50 +0100

    feat: sum of dice and add method change position and bonusMagic

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        Player player = new Player("User1",1,false);
+        Player joel = new Player("Joel",1,false);
         Dice dice1 = new Dice();
-        System.out.println(dice1.getRandom());
+
+        Dice dice2 = new Dice();
+        joel.rollDice(dice1.getRandom(),dice2.getRandom());
+        System.out.println(joel.sum);
     }
+
+
 }
--- src/Player.java
+++ src/Player.java
@@ -2,14 +2,30 @@ public class Player {
     public String name;
     public int position;
     public Boolean hasPlayed;
+
+    public int sum;
+
     Player(String name,int position,Boolean hasPlayed){
     this.name=name;
     this.position=position;
     this.hasPlayed=hasPlayed;
+
     }
 
     public int rollDice(int a, int b){
-        return 0;
+        sum= a+b;
+        System.out.println(a+" "+b);
+        return sum;
+    }
+
+    public int changePosition(int c){
+        position+=c;
+        return c;
+    }
+
+    public int bonusMagic(int d ){
+        position = d;
+        return d;
     }
 
 
