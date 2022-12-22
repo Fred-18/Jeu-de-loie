3d8464a15b0a277c7633b2ad40ce7344c9bd0d90
anharaAkter <anhara.susoma@gmail.com>
2022-12-22 13:18:13 +0100

    initiale project

---
+++ src/Dices.java
@@ -0,0 +1,4 @@
+public class Dices {
+    public String dice1;
+    public String dice2;
+}
---
+++ src/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+    public String name;
+    public int position;
+    public Boolean hasPlayed;
+    public int rollDice(int a, int b){
+        return 0;
+    }
+
+
+}
