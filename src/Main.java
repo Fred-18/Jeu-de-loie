f7306ccc34dc96bb981f8c69aae82860c96ab149
Morwen11 <86191581+Morwen11@users.noreply.github.com>
2022-12-23 11:41:18 +0100

    Add boardgame and player position

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,45 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        Player joel = new Player("Joel",1,false);
+        Player joel = new Player("Joel",0,false);
         Dice dice1 = new Dice();
 
         Dice dice2 = new Dice();
-        joel.rollDice(dice1.getRandom(),dice2.getRandom());
-        System.out.println(joel.sum);
-    }
+        while(joel.sum<12){
+            joel.rollDice(dice1.getRandom(),dice2.getRandom());
+            System.out.println(joel.sum);
+            joel.changePosition(joel.sum);
+            Board(joel.position);
+        }
+
+     }
+       public static void Board(int x){
+        for (int i= 0;i<=63;i++){
+            System.out.print(i);
+
+          if (i<10) {
+                System.out.print("  ");
+            }
+          else{
+              System.out.print(" ");
+          }
+        }
+        System.out.println("");
+
+           for (int y=0;y<=63;y++){
+               if (x==y){
+                   System.out.print("z");
+               }
+
+                   System.out.print("   ");
+
+               }
+
+       }
+
+       }
 
 
-}
+
+
+
